4f15dbfccf64c84995e27445a970e019f6027568
MartinOrtega22 <ortega.martin.14am@gmail.com>
2024-05-14 19:01:18 -0600

    CRUD Usuarios y Sucursales en php

---
+++ CRUD Usuarios.php
@@ -0,0 +1,93 @@
+
+<?php
+            // Variables base de datos
+            $host = "localhost";
+            $usuario = "root"; 
+            $contra = "megustaelcereal"; 
+            $bd = "farmacia"; 
+
+            // Conexión
+            $conn = new mysqli($host, $usuario, $contra, $bd);
+
+            // VerificA conexión
+            if ($conn->connect_error) {
+                die("Error de conexión: " . $conexion->connect_error);
+            }
+
+            
+        ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sucursales</title>
+    <link rel="stylesheet" href="css/CRUDUsuarios.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.0/font/bootstrap-icons.css">
+
+</head>
+<body>
+        
+    <div class="container">
+        <ul class="nav justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Productos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Nosotros</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="bi bi-geo-alt"></i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="bi bi-cart4"></i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="bi bi-person-circle"></i></i></a>
+            </li>
+        </ul>
+
+        <a href="agregar.php">Agregar Usuario</a>
+        <table class="table">
+            <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Dirección</th>
+            <th>Correo</th>
+            <th>Teléfono</th>
+            <th>Rol</th>
+            <th>Acciones</th>
+            </tr>
+        <?php
+        $sql = "SELECT IdUsuario, NombreUsuario, DireccionUsuario, CorreoUsuario, TelefonoUsuario, DescripcionRol FROM usuario JOIN rol where RolUsuario=IdRol";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>" . $row["IdUsuario"]. "</td>";
+                echo "<td>" . $row["NombreUsuario"]. "</td>";
+                echo "<td>" . $row["DireccionUsuario"]. "</td>";
+                echo "<td>" . $row["CorreoUsuario"]. "</td>";
+                echo "<td>" . $row["TelefonoUsuario"]. "</td>";
+                echo "<td>" . $row["DescripcionRol"]. "</td>";
+                echo '<td><a href="editar.php?id=' . $row["IdUsuario"] . '" id="btnEditar"><img src="img/editar.png"></a>  <a href="eliminar.php?id=' . $row["IdUsuario"] . '" id="btnEliminar"><img src="img/eliminar.png"></a></td>';
+                echo "</tr>";
+            }
+        } else {
+            echo "0 resultados";
+        }
+        $conn->close();
+        ?>
+        </table>
+        
+
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ sucursales.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sucursales</title>
+    <link rel="stylesheet" href="css/sucursales.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.0/font/bootstrap-icons.css">
+
+</head>
+<body>
+    <div class="container">
+        <ul class="nav justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Productos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Nosotros</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="bi bi-geo-alt"></i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="bi bi-cart4"></i></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="bi bi-person-circle"></i></i></a>
+            </li>
+        </ul>
+    
+        <?php
+            // Configuración de la conexión a la base de datos
+            $host = "localhost";
+            $usuario = "root"; 
+            $contra = "megustaelcereal"; 
+            $bd = "farmacia"; 
+
+            // Conexión
+            $conn = new mysqli($host, $usuario, $contra, $bd);
+
+            // VerificA conexión
+            if ($conn->connect_error) {
+                die("Error de conexión: " . $conn->connect_error);
+            }
+        ?>
+
+        <?php
+        // consulta
+            $sql = "SELECT NombreSucursal, DireccionSucursal, TelefonoSucursal FROM sucursal";
+            $resultado = $conn->query($sql);
+
+    
+            if ($resultado->num_rows > 0) {
+        // Por cada resulatado te muestra una card
+                while ($fila = $resultado->fetch_assoc()) {
+                    echo '<div class="sucursal">';
+                    echo '<h2>' . $fila['NombreSucursal'] . '</h2>';
+                    echo '<p><strong>Dirección:</strong> ' . $fila['DireccionSucursal'] . '</p>';
+                    echo '<p><strong>Teléfono:</strong> ' . $fila['TelefonoSucursal'] . '</p>';
+                    echo '</div>';
+            }
+            } else {
+                echo "No se encontraron sucursales.";
+            }
+
+            $conexion->close();
+        ?>
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+</body>
+</html>
