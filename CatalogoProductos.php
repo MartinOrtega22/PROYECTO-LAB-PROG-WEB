7cd60a13f6fabdf60f568d59665658551c4a82c0
lesliMagana <167039288+lesliMagana@users.noreply.github.com>
2024-05-18 14:32:46 -0600

    Interfaz catalogo de productos

--- CatalogoProductos.php
+++ CatalogoProductos.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Catalogo de productos</title>
+    <title>Catálogo de productos</title>
     <link rel="stylesheet" href="css/CatalogoProductos.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.0/font/bootstrap-icons.css">
-
 </head>
 
 <body>
@@ -34,6 +33,7 @@
                 <a class="nav-link" href="#"><i class="bi bi-person-circle"></i></i></a>
             </li>
         </ul>
+
         <?php
         // Configuración de la conexión a la base de datos
         $host = "localhost";
@@ -42,17 +42,17 @@
         $bd = "farmacia";
 
         // Conexión
-        $conn = new mysqli($host, $usuario, $contra, $bd);
+        $conexion = new mysqli($host, $usuario, $contra, $bd);
 
         // VerificA conexión
-        if ($conn->connect_error) {
-            die("Error de conexión: " . $conn->connect_error);
+        if ($conexion->connect_error) {
+            die("Error de conexión: " . $conexion->connect_error);
         }
-        ?>
-        <?php
 
         // Consulta para obtener los productos
-        $consulta = "SELECT IdProducto, NombreProducto, DescripcionProducto, PrecioProducto, CategoriaProducto FROM producto";
+        $consulta = "SELECT IdProducto, NombreProducto, DescripcionProducto, PrecioProducto, DescripcionCategoria AS CategoriaProducto, ImagenProducto
+                     FROM producto
+                     INNER JOIN categorias ON CategoriaProducto = IdCategoria";
         $resultado = mysqli_query($conexion, $consulta);
 
         // Mostrar los productos
@@ -61,10 +61,11 @@
             while ($producto = mysqli_fetch_assoc($resultado)) {
                 echo '<div class="col mb-4">';
                 echo '<div class="card">';
-                echo '<img src="' . $producto['imagen'] . '" class="card-img-top" alt="' . $producto['nombre'] . '">';
+                echo '<img src="' . $producto['ImagenProducto'] . '" class="card-img-top" alt="' . $producto['NombreProducto'] . '">';
                 echo '<div class="card-body">';
-                echo '<h5 class="card-title">' . $producto['nombre'] . '</h5>';
-                echo '<p class="card-text">' . $producto['descripcion'] . '</p>';
+                echo '<h5 class="card-title">' . $producto['NombreProducto'] . '</h5>';
+                echo '<p class="card-text">' . $producto['DescripcionProducto'] . '</p>';
+                echo '<p class="card-text">Categoría: ' . $producto['CategoriaProducto'] . '</p>';
                 echo '<a href="#" class="btn btn-primary">Ver detalle</a>';
                 echo '</div>';
                 echo '</div>';
@@ -78,9 +79,12 @@
         $conexion->close();
         ?>
     </div>
+
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
         crossorigin="anonymous"></script>
+
 </body>
 
 </html>
